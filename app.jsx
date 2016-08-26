4e9d8f9eda8f9c326735a8d8320783eeadbcb036
Jon Garrett <jonathan.garrett@gmail.com>
2016-08-25 18:03:44 -0700

    Move dummy data, refactor app to pseudoclassical and list to functional

--- app.jsx
+++ app.jsx
@@ -1,45 +1,46 @@
-var photos = [
-  {url: './mockups/MockUp0.png', title: 'Mock Up 0 index', rating: '3.5 stars'},
-  {url: './mockups/MockUp1.png', title: 'Mock Up 1', rating: '3.5 stars'},
-  {url: './mockups/MockUp2.png', title: 'Another Mock Up', rating: '3.5 stars'},
-  {url: './mockups/MockUp3.png', title: 'Still More Mock Ups!', rating: '3.5 stars'},
-  {url: './mockups/MockUp4.png', title: 'They keep getting better!', rating: '3.5 stars'},
-];
 class App extends React.Component {
   constructor (props) {
+    // Extend react component by passing props to super
     super(props);
+    // Set initial state of app
+    this.state = {
+      curPhoto: null,
+      photoList: props.photos
+    };
+    // Bind the click event listener function to this scope
+    this.photoClick = this.photoClick.bind(this);
   }
-
+  // Declare function to change state when list item is clicked
+  photoClick (photo) {
+    this.setState({curPhoto: photo});
+  }
   render () {
     return (
       <div>
-        <PhotoList photos={photos} />
+        <PhotoList photos={this.state.photoList} />
       </div>
-    ) 
+    );
   }
-
 }
-
-class PhotoList extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = props;
-  }
-
-  render () {
-    return (
-      <ul className="photo-list">
-        {this.state.photos.map(photo => 
-          <PhotoEntry photo={photo} />
-        )}
-      </ul>
-    )
-  }
-
+var PhotoList = (props) => {
+  return (
+    <ul className="photo-list">
+      {props.photos.map(photo => 
+        <PhotoEntry photo={photo} />
+      )}
+    </ul>
+  );
 }
-
 var PhotoEntry = (props) => {
   return (
     <li>{props.photo.title}</li>
-  )
-}
+  );
+};
+var PhotoView = (props) => {
+  return (
+    <div>
+      <img src={props.url} />
+      <p>{props.title}</p>
+    </div>
+  );
+};
